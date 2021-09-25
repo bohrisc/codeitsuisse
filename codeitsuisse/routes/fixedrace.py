41bc80ae3d7dd397b9d84372a07a5511100e637b
bohrisc <ccyboris@gmail.com>
2021-09-25 22:18:00 +0800

    Update fixedrace.py

--- codeitsuisse/routes/fixedrace.py
+++ codeitsuisse/routes/fixedrace.py
@@ -10,7 +10,15 @@ logger = logging.getLogger(__name__)
 
 @app.route('/fixedrace', methods=['POST'])
 def fixedrace():
-    data = request.get_data()
+    data = request.get_data(as_text=True)
     logging.info("data sent for evaluation {}".format(data))
     temp = data.split(",")
-    return temp
+    random.shuffle(temp)
+    result = ""
+    for i in range(len(temp)):
+        result += temp[i]
+        if i == len(temp)-1:
+            pass
+        else:
+            result += ","
+    return result
