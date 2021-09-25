97dc1a45f3855d9ed4e309cf532814396c4f4628
bohrisc <ccyboris@gmail.com>
2021-09-25 12:24:07 +0800

    Update asteroid.py

--- codeitsuisse/routes/asteroid.py
+++ codeitsuisse/routes/asteroid.py
@@ -7,14 +7,24 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-@app.route('/square', methods=['POST'])
+def asteroid(request):
+
+
+@app.route('/asteroid', methods=['POST'])
 def evaluate():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("input")
-    result = inputValue * inputValue
+    result = 1
     logging.info("My result :{}".format(result))
     return json.dumps(result)
 
 
+
+def main():
+    for i in range(len(input_list)):
+        for j in range(input_list[i]):
+            
+
+            input_list[]
 
